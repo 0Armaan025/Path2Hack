509f56c7d8ba36adb442f486601f3db235bb3f4d
Armaan <armaan33000@gmail.com>
2024-11-08 22:41:43 +0530

    added navbar and shadcn ui

---
+++ client/path-2-hack/src/home/HomePage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./homepage.css";
+import Navbar from "@/components/navbar/Navbar";
+
+type Props = {};
+
+const HomePage = (props: Props) => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default HomePage;
